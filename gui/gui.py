6dbfe3fa65ca269bb6d2341a14b98a20793b691f
haydendoyle1 <89240752+haydendoyle1@users.noreply.github.com>
2023-05-18 15:20:29 +1000

    GUI comms with BSU

--- gui/gui.py
+++ gui/gui.py
@@ -5,121 +5,188 @@ import numpy as np
 from tkinter import messagebox
 import serial
 import serial.tools.list_ports
+import threading, queue
+
+history = []
+current_button = None
+
+button_queue = queue.Queue()
+
+def main(ser):
+    button_queue.queue.clear()
+    ser.write(b"scan f\n")
+    print(ser.readline().decode('utf-8'))
+    print(ser.readline().decode('utf-8'))
+    print(ser.readline().decode('utf-8'))
+    ser.write(b"scan o\n")
+    print(ser.readline().decode('utf-8'))
+    print(ser.readline().decode('utf-8'))
+    print(ser.readline().decode('utf-8'))
+    while True:
+
+        # Dequeue a button index
+        button_idx = button_queue.get()
+        
+        # Process the button index as needed
+        # For example, print the index
+        print(f"Button {button_idx} pressed")
+
+        # LEFT button
+        if button_idx == 0:
+            ser.write(b"gesture 0\n")
+
+        # UP button
+        elif button_idx == 1:
+            ser.write(b"gesture 1\n")
+
+        # DOWN button
+        elif button_idx == 2:
+            ser.write(b"gesture 2\n")
+
+        # RIGHT button
+        elif button_idx == 3:
+            ser.write(b"gesture 3\n")
+
+        # CW button
+        elif button_idx == 4:
+            ser.write(b"gesture 4\n")
+
+        # CCW button
+        elif button_idx == 5:
+            ser.write(b"gesture 5\n")
+
+        # RESET button
+        elif button_idx == 6:
+            ser.write(b"reset\n")
+
+        print(ser.readline().decode('utf-8'))
+        print(ser.readline().decode('utf-8'))
+        print(ser.readline().decode('utf-8'))
 
 
-class ButtonHistoryGUI:
-    def __init__(self):
-        self.history = []
-        self.current_button = None
-        self.root = tk.Tk()
-        self.root.title("Button History GUI")
-        
-        self.plot_frame = tk.Frame(self.root)
-        self.plot_frame.pack(side=tk.LEFT, padx=10, pady=10)
+def button_clicked(idx):
+    global current_button
+    global history
 
-        bottom_frame = tk.Frame(self.root)
-        bottom_frame.pack(side=tk.TOP, pady=10)
-        
-        self.button_frame = tk.Frame(self.root)
-        self.button_frame.pack(padx=10, pady=10)
-        
-        self.history_frame = tk.Frame(self.root)
-        self.history_frame.pack(side=tk.BOTTOM, padx=10, pady=10)
-        
-        self.buttons = []
-        
-        symbols = ['←', '↑', '↓', '→','↻', '↺']
-        
-        for i in range(4):
-            button = tk.Button(self.button_frame, text=symbols[i], font=('Arial', 16), command=lambda idx=i: self.button_clicked(idx))
-            button.grid(row=0, column=i, padx=5, pady=5)
-            self.buttons.append(button)
-        
-        for i in range(4, 6):
-            button = tk.Button(self.button_frame, text=symbols[i], font=('Arial', 16), command=lambda idx=i: self.button_clicked(idx))
-            button.grid(row=1, column=i-3, padx=5, pady=5)
-            self.buttons.append(button)
-        
-        self.history_label = tk.Label(self.history_frame, text="History:")
-        self.history_label.pack()
-        
-        self.history_text = tk.Text(self.history_frame, height=4, width=10, font=('Arial', 16))
-        self.history_text.pack()
-        
-        # Create the scatter plots
-        fig = Figure(figsize=(5, 5), dpi=100)
-        ax1 = fig.add_subplot(211)
-        ax2 = fig.add_subplot(212)
-        self.plot_scatter(ax1)
-        self.plot_scatter(ax2)
-
-        # Add the LED RESET and CONNECT buttons        
-        reset_button = tk.Button(bottom_frame, text="RESET LED", font=('Arial', 16))
-        reset_button.pack(side=tk.TOP, padx=1, pady=10)
-        
-        connect_button = tk.Button(bottom_frame, text="CONNECT", font=('Arial', 16), command=self.connect_to_com4)
-        connect_button.pack(side=tk.TOP, padx=1, pady=10)
-        
-        # Add the plots to the GUI
-        canvas = FigureCanvasTkAgg(fig, master=self.plot_frame)
-        canvas.draw()
-        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-
-
-        
-    def button_clicked(self, idx):
-        if self.current_button is not None:
-            self.buttons[self.current_button].config(bg='white')
-        
-        self.buttons[idx].config(bg='red')
-        self.current_button = idx
-        self.history.append(self.buttons[idx]['text'])
-        
-        if len(self.history) > 4:
-            self.history = self.history[-4:]
-        
-        self.update_history_text()
-        
-    def update_history_text(self):
-        self.history_text.delete(1.0, tk.END)
-        for item in self.history:
-            self.history_text.insert(tk.END, item + "\n")
+    if current_button is not None:
+        buttons[current_button].config(bg='white')
     
-    def plot_scatter(self, ax):
-        # Generate random points for the scatter plot
-        x = np.random.rand(50)
-        y = np.random.rand(50)
-        
-        ax.scatter(x, y, c='blue')
-        ax.set_xlabel('X')
-        ax.set_ylabel('Y')
-        ax.set_title('Scatter Plot')
-        
-    def run(self):
-        self.root.mainloop()
+    buttons[idx].config(bg='red')
+    current_button = idx
+    history.append(buttons[idx]['text'])
+    
+    if len(history) > 4:
+        history = history[-4:]
+    
+    update_history_text()
 
-    def connect_to_com4(self):
-        # Simulating connection to COM4
-        # Replace this with your actual connection logic
-        connected = True  # Set this based on the connection status
+    # Enqueue the button index
+    button_queue.put(idx)
+    
+def update_history_text():
+    history_text.delete(1.0, tk.END)
+    for item in history:
+        history_text.insert(1.0, item + "\n")
 
-        ports = serial.tools.list_ports.comports()
-        for port in ports:
-            print(f"Port: {port.device}, Description: {port.description}")
+def plot_scatter(ax):
+    # Generate random points for the scatter plot
+    x = np.random.rand(50)
+    y = np.random.rand(50)
+    
+    ax.scatter(x, y, c='blue')
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_title('Scatter Plot')
+    
+BSU_connected = "Failed to connect to BSU"
+mmW_connected = "Failed to connect to mmW"
+
+def connect_to_com4():
+    # Connection to BSU and mmW
+    global BSU_connected, mmW_connected
+
+    ports = serial.tools.list_ports.comports()
+    for port in ports:
+        print(f"Port: {port.device}, Description: {port.device}")
+        if port.description == "USB Serial Device (" + port.device + ")":
             try:
                 ser = serial.Serial(port.device, baudrate=115200, timeout=1)
-                print("connected to" + port.device)
-                ser.write(b'\n')
-                print(ser.readline().decode('utf-8'))
+                print("connected to " + port.device)
+            except serial.serialutil.SerialException:
+                print("Could not connect to " + port.device)
+                continue
+            ser.write(b"\n")
+            a = ser.readline().decode('utf-8')
+            a = ser.readline().decode('utf-8')
+            if a == '\x1b[1;32mBSU_SHELL:~$ \x1b[m':
+                print("Starting BSU Thread")
+                bsu_thread = threading.Thread(target=main, args=(ser,))
+                bsu_thread.start()
+                BSU_connected = "Connected to BSU on " + port.device
+                
+            else:
+                ser.close()
 
-            except:
-                print("could not connect to" + port.device)
+    
+    messagebox.showinfo("Connection Status", BSU_connected + "\n" + mmW_connected)
 
-        
-        if connected:
-            messagebox.showinfo("Connection Status", "Connected to COM4")
-        else:
-            messagebox.showinfo("Connection Status", "Failed to connect to COM4")
 
-gui = ButtonHistoryGUI()
-gui.run()
+
+
+root = tk.Tk()
+root.title("Button History GUI")
+
+plot_frame = tk.Frame(root)
+plot_frame.pack(side=tk.LEFT, padx=10, pady=10)
+
+bottom_frame = tk.Frame(root)
+bottom_frame.pack(side=tk.TOP, pady=10)
+
+button_frame = tk.Frame(root)
+button_frame.pack(padx=10, pady=10)
+
+history_frame = tk.Frame(root)
+history_frame.pack(side=tk.BOTTOM, padx=10, pady=10)
+
+buttons = []
+
+symbols = ['←', '↑', '↓', '→','↻', '↺']
+
+for i in range(4):
+    button = tk.Button(button_frame, text=symbols[i], font=('Arial', 16), command=lambda idx=i: button_clicked(idx))
+    button.grid(row=0, column=i, padx=5, pady=5)
+    buttons.append(button)
+
+for i in range(4, 6):
+    button = tk.Button(button_frame, text=symbols[i], font=('Arial', 16), command=lambda idx=i: button_clicked(idx))
+    button.grid(row=1, column=i-3, padx=5, pady=5)
+    buttons.append(button)
+
+history_label = tk.Label(history_frame, text="History:")
+history_label.pack()
+
+history_text = tk.Text(history_frame, height=4, width=10, font=('Arial', 16))
+history_text.pack()
+
+# Create the scatter plots
+fig = Figure(figsize=(5, 5), dpi=100)
+ax1 = fig.add_subplot(211)
+ax2 = fig.add_subplot(212)
+plot_scatter(ax1)
+plot_scatter(ax2)
+
+# Add the LED RESET and CONNECT buttons        
+reset_button = tk.Button(bottom_frame, text="RESET LED", font=('Arial', 16), command=lambda:button_queue.put(6))
+reset_button.pack(side=tk.TOP, padx=1, pady=10)
+
+connect_button = tk.Button(bottom_frame, text="CONNECT", font=('Arial', 16), command=connect_to_com4)
+connect_button.pack(side=tk.TOP, padx=1, pady=10)
+
+# Add the plots to the GUI
+canvas = FigureCanvasTkAgg(fig, master=plot_frame)
+canvas.draw()
+canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+
+root.mainloop()
+
