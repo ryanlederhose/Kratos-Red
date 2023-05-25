8230307032394ac2949ccf1633ceccbd0f2409b5
haydendoyle1 <89240752+haydendoyle1@users.noreply.github.com>
2023-05-25 16:53:00 +1000

    Update datalog.py

--- gui/datalog.py
+++ gui/datalog.py
@@ -1,7 +1,7 @@
 import influxdb_client, os, time
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
-import time
+
 
 INFLUXDB_TOKEN="wJmCnn5sL-6yBFoiHlhQcylFfxtwkLMyc6qPXUYTF4gBe95MFlNZF7qWiAqHMFrOCSdpvDo5XCCIPDDwIO3M3Q=="
 org = "CSSE4011 P3B"
@@ -11,17 +11,22 @@ bucket="test4011"
 
 write_api = client.write_api(write_options=SYNCHRONOUS)
 
-
-
-def influxSend(queueLogs):
+def influxSend():
+    global queueLogs
     while True:
+
+        first = time.time()
+
         dataObj = queueLogs.get()
 
-        X = -dataObj["x"]
+        X = dataObj["x"]
         Y = dataObj["y"]
         V = dataObj["velocity"]
+        Z = dataObj["z"]
         numObjs = dataObj["numObj"]
         frameNum = dataObj["frNum"]
+
+        
 
         for i in range(0, numObjs):
             point = (
@@ -34,4 +39,13 @@ def influxSend(queueLogs):
                 )
             
             write_api.write(bucket=bucket, org=org, record=point)
-        
+            print("Frame Number - " + str(frameNum))
+            print("Object Number - " + str(i))
+            print("V - " + str(V[i]))
+            print("X - " + str(X[i]))
+            print("Y - " + str(Y[i]))
+
+        end = time.time()
+        print("Time: " + str(end - first))
+        break
+            
